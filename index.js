2cb0fd9a67e64e87649c33e82f7040775c59e656
yunoprogramming <matthewhosangrose@gmail.com>
2023-05-03 14:16:34 -0400

    openai backend

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+// enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/openai', require('./routes/openaiRoutes'));
+
+app.listen(port, () => console.log(`server started on port ${port}`));
