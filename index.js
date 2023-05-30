52ca3f911b1c1db5644611303ec8bb05dfe0ffa1
yunoprogramming <matthewhosangrose@gmail.com>
2023-05-30 19:16:37 -0400

    build

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
-const dotenv = require('dotenv').config();
 const port = process.env.PORT || 3000;
 
 const app = express();
